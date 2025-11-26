b3ba4c97712872152942757b0efcfa513047c63d
toAlbert <77997269+toAlbert@users.noreply.github.com>
2025-11-26 20:45:29 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,28 +1,32 @@
-import os
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
-TOKEN = os.getenv("7933621950:AAEMx0oUWK27zx9NDrYyvRGHhimemAh5pic")
+TELEGRAM_TOKEN = os.getenv("7933621950:AAEMx0oUWK27zx9NDrYyvRGHhimemAh5pic")
 CHAT_ID =  os.getenv("6873763875")
 
 
+@app.route("/", methods=["GET"])
+def home():
+    return "Webhook funcionando"
 
-@app.post("/alert")
+@app.route("/alert", methods=["POST"])
 def alert():
     data = request.json
 
-    # Mensaje simple (puede ser dinámico según lo que envíe TradingView)
-    message = f"🚨 Alerta recibida:\n\n{data}"
+    message = f"Alerta recibida:\n{data}"
 
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message
-    }
+    r = requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+        json={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+    )
 
-    # Enviar a Telegram
-    r = requests.post(url, json=payload)
-
-    return jsonify({"status": "ok", "telegram_status": r.status_code})
+    return jsonify({
+        "status": "ok",
+        "telegram_status": r.status_code
+    })
